d3187a33a3c283a6427c804d4a6be40caf7e8656
natsm <natsmarinho@gmail.com>
2023-08-23 17:49:03 -0300

    feat: estudo de repeticoes

---
+++ js/repeticoes/ambiente.js
@@ -0,0 +1,13 @@
+let contador = 1;
+
+//estrutura de repetição com teste lógico no início
+/*while (contador < 6) {
+    console.log(`Passo ${contador}`);
+    contador++;
+}*/
+
+//estrutura de repetição com teste lógico no final 
+do {
+    console.log(`Passo ${contador}`);
+    contador++
+} while (contador < 5)

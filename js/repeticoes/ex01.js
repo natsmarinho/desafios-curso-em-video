0221542ad633eb99309189dbb228c941f801ed7e
natsm <natsmarinho@gmail.com>
2023-09-06 19:43:47 -0300

    feat: iniciando funcionalidade com js

---
+++ js/repeticoes/ex01.js
@@ -0,0 +1,8 @@
+const numInicio = document.querySelector("#inicio");
+const numFim = document.querySelector("#fim");
+const passos = document.querySelector("#passo");
+const btnGerar = document.querySelector("button").addEventListener("click", gerarContagem)
+
+function gerarContagem() {
+    
+}

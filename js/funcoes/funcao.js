b5cee07592dc52253229b94a492743b4749ae54e
natsm <natsmarinho@gmail.com>
2023-09-15 17:36:10 -0300

    feat: aplicacoes com objetivo de aprender funcoes

---
+++ js/funcoes/funcao.js
@@ -0,0 +1,11 @@
+//Funções são ações executadas assim que são chamadas ou em decorrência de algum evento
+
+function parImpar(n) {
+    if(n%2 == 0) {
+       return `${n} é par.` 
+    } else {
+        return `${n} é par.`
+    }
+}
+
+console.log(parImpar(265))
---
+++ js/funcoes/soma.js
@@ -0,0 +1,11 @@
+function soma(num1=0, num2=0) {
+    return num1 + num2;
+}
+
+console.log(soma(2, 6))
+
+let s = function(x, y) {
+    return x + y
+}
+
+console.log(s(5,7))

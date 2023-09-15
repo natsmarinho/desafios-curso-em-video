678af860115bf055d6670bc50348ab4a7382031d
natsm <natsmarinho@gmail.com>
2023-09-15 17:02:28 -0300

    feat: Manipulando arrays com valores string

---
+++ js/array/words-array.js
@@ -0,0 +1,4 @@
+let words = ["Banana", "Abacate", "cacau", "damasco"]
+console.log(`As frutas da nossa array são: ${words}`);
+words.sort();
+console.log(`Agora as frutas em ordem alfabetica ${words}`)

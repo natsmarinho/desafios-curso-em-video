b9a1bd02e5b80bb89699c9bb8e97b3feaa54cc0b
natsm <natsmarinho@gmail.com>
2023-09-27 18:55:36 -0300

    feat: adicionando valores ao select, mensagem para qtd numero, maior menor, soma e media dos valores

--- js/exercicio/script.js
+++ js/exercicio/script.js
@@ -1,7 +1,9 @@
 const inputNum = document.querySelector("input#numero");
 Number(inputNum)
 const btnAdicionar = document.querySelector("#adicionar-numero").addEventListener("click", AdicionaNum);
-const mensagem = document.querySelector("div#res");
+const mensagem = document.querySelector("div#msg-error");
+const resultado = document.querySelector("#res")
+mensagem.style.color = "red";
 let containerNumAdicionado = document.querySelector("select#numeros-adicionados");
 let valores = [];
 
@@ -23,15 +25,60 @@ function inLista(n, l) {
 
 function AdicionaNum() {
     if(isNum(inputNum.value) && !inLista(inputNum.value, valores)){
-        
+        valores.push(Number(inputNum.value))
+        let item = document.createElement('option');
+        item.text= `Valor ${inputNum.value} adicionado!`;
+        containerNumAdicionado.appendChild(item);
+        mensagem.innerHTML = '';
+        res.innerHTML = '';
+    } else {
+        mensagem.innerHTML = "Número já adicionado a lista!";
     }
 
     if(inputNum.value == "") {
-        mensagem.innerHTML += "Não foi possível adicionar pois não foi inserido nenhum número!"
+        mensagem.innerHTML = "Não foi possível adicionar pois não foi inserido nenhum número!"
     } else if(inputNum.value > 100) {
-        mensagem.innerHTML += "Número maior que 100."
+        mensagem.innerHTML = `O número ${inputNum.value} é maior que 100.`
     }
 
     inputNum.value = "";
+    inputNum.focus();
     containerNumAdicionado.innerHTML += inputNum.value;
+}
+
+const btnFinaliza = document.querySelector("#btn-finaliza").addEventListener("click", finalizar);
+
+function finalizar() {
+    mensagem.innerHTML = '';
+    if (valores.length == 0) {
+        mensagem.innerHTML = "Adicione valores antes de finalizar!"
+    } else {
+        let totalVetores = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        let soma = 0;
+        let media = 0;
+
+        for(let pos in valores) {
+            soma += valores[pos];
+            if(valores[pos] > maior) 
+            maior = valores[pos]
+            if(valores[pos] < menor)
+            menor = valores[pos]
+        }
+
+        mensagem.innerHTML = '';
+        media = soma / totalVetores;  
+
+        if (valores.length == 1) {
+            res.innerHTML += `<p>Ao todo temos: ${totalVetores} número cadastrado.`
+        } else {
+            res.innerHTML += `<p>Ao todo temos: ${totalVetores} números cadastrados.</p>`
+            res.innerHTML += `<p>O maior valor informado foi ${maior}.</p>`;
+            res.innerHTML += `<p> O menor número informado foi ${menor}.`;
+            res.innerHTML += `<p>Somando todos os valores, temos: ${soma}.`
+            res.innerHTML += `<p>A média dos valores digitatos é ${media}.</p>`
+        }
+    }
+    
 }
